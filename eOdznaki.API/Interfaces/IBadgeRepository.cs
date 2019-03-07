c97b075d2c5d064f1c0865643a34e178c00c4398
empyty <bartoszlistwan@gmail.com>
2019-03-07 11:45:27 +0100

    Created Interfaces folder and an interface for badge repository

---
+++ eOdznaki.API/Interfaces/IBadgeRepository.cs
@@ -0,0 +1,13 @@
+﻿using eOdznaki.Models.Badges;
+using System.Collections.Generic;
+
+namespace eOdznaki.Persistence.Repositories
+{
+    public interface IBadgeRepository
+    {
+        IEnumerable<Badge> GetAllBadges();
+        IEnumerable<Badge> GetBadgesByType(string type);
+
+        Badge AddBadge(Badge newBadge);
+    }
+}
