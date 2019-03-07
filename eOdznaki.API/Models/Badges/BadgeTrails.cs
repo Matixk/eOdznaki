6b39c498c7a00a1d1e139626e7c7067b0a8e4a6f
empyty <bartoszlistwan@gmail.com>
2019-03-07 10:02:43 +0100

    Added basic badge class for trails; Badge is achieved by completing trails, each trail gives specific points

---
+++ eOdznaki.API/Models/Badges/BadgeTrails.cs
@@ -0,0 +1,20 @@
+﻿using eOdznaki.Models.Trails;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Models.Badges
+{
+    public class BadgeTrails
+    {
+        [Required]
+        public int BadgeLevel { get; set; }
+        [Required]
+        public int PointsAquired { get; set; }
+        [Required]
+        public IDictionary<int, IEnumerable<int>> LevelRequirements { get; private set; }
+        [Required]
+        public IEnumerable<int> Requirements { get; private set; }
+        [Required]
+        public IEnumerable<Trail> Trails { get; private set; }
+    }
+}
