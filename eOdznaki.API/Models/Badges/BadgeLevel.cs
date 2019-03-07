948d87b8faf594b1e512f51a85308a648e861021
empyty <bartoszlistwan@gmail.com>
2019-03-07 15:11:39 +0100

    Added badge level class to connect badges with requirement in database

---
+++ eOdznaki.API/Models/Badges/BadgeLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Models.Badges
+{
+    public class BadgeLevel
+    {
+        public int Id { get; set; }
+        [Required]
+        public BadgeRequirements BadgeRequirements { get; }
+    }
+}
