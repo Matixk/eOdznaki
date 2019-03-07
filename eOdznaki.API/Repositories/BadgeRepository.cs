6c5caad45fcfe256a876adddfdc6815483acb819
empyty <bartoszlistwan@gmail.com>
2019-03-07 15:14:45 +0100

    Added constructor and implemented basic functions

--- eOdznaki.API/Repositories/BadgeRepository.cs
+++ eOdznaki.API/Repositories/BadgeRepository.cs
@@ -1,12 +1,24 @@
 ﻿using eOdznaki.Models.Badges;
+using eOdznaki.Persistence;
 using eOdznaki.Persistence.Repositories;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace eOdznaki.Repositories
 {
     public class BadgeRepository : IBadgeRepository
     {
+        private readonly DataContext context;
+        private readonly ILogger<BadgeRepository> logger;
+
+        public BadgeRepository(DataContext context, ILogger<BadgeRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
         public Badge AddBadge(Badge newBadge)
         {
             throw new NotImplementedException();
@@ -14,12 +26,15 @@ namespace eOdznaki.Repositories
 
         public IEnumerable<Badge> GetAllBadges()
         {
-            throw new NotImplementedException();
+            logger.LogInformation("GetAllBadges was called");
+            return context.Badges.ToList();
         }
 
         public IEnumerable<Badge> GetBadgesByType(string type)
         {
-            throw new NotImplementedException();
+            logger.LogInformation($"GetBadgesByType was called with parameter {type}");
+            // Only an example, create a switch statement checking type
+            return context.Badges.OfType<BadgeDrops>().ToList();
         }
     }
 }
