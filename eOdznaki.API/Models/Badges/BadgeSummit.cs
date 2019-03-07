097694f5700cf171734469c7bf01be1079d588a5
empyty <bartoszlistwan@gmail.com>
2019-03-07 10:01:31 +0100

    Added basic badge class for summits; Badge is achieved by reaching specific set of summits

---
+++ eOdznaki.API/Models/Badges/BadgeSummit.cs
@@ -0,0 +1,12 @@
+﻿using eOdznaki.Models.Locations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Models.Badges
+{
+    public class BadgeSummit : Badge
+    {
+        [Required]
+        public IEnumerable<Location> SummitLocations { get; private set; };
+    }
+}
