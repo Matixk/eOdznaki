6f73e42b3c30495315ff69011eaaa52bf74eb5bf
empyty <bartoszlistwan@gmail.com>
2019-03-07 15:12:05 +0100

    Added badgeRequirements class to be accesed by badge level in database

---
+++ eOdznaki.API/Models/Badges/BadgeRequirements.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Models.Badges
+{
+    public class BadgeRequirements
+    {
+        public int Id { get; private set; }
+        [Required]
+        public int Requirement { get; private set; }
+    }
+}
