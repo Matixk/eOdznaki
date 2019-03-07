5fa8daf5fd2a3f95dcf2b16e2729595a279aaf74
empyty <bartoszlistwan@gmail.com>
2019-03-07 10:00:00 +0100

    Added basic badge class for drops, for badges that are given for achieving specific total height

---
+++ eOdznaki.API/Models/Badges/BadgeDrops.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Models.Badges
+{
+    public class BadgeDrops : Badge
+    {
+        [Required]
+        public int BadgeLevel { get; set; }
+        [Required]
+        public IDictionary<int, IEnumerable<int>> LevelRequirements { get; private set; }
+        [Required]
+        public IEnumerable<int> Requirements { get; private set; }
+    }
+}
