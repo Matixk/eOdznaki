f504f82918a156b9a9dfefc1af667a798a944271
empyty <bartoszlistwan@gmail.com>
2019-03-07 11:46:44 +0100

    Created controllers folder; added badges controler with just a constructor

---
+++ eOdznaki.API/Controllers/BadgesController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using eOdznaki.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eOdznaki.Controllers
+{
+    public class BadgesController : ControllerBase
+    {
+        private readonly IBadgeRepository repository;
+        private readonly ILogger<BadgesController> logger;
+        private readonly IMapper mapper;
+
+        public BadgesController(IBadgeRepository repository,
+            ILogger<BadgesController> logger, IMapper mapper)
+        {
+            this.repository = repository;
+            this.logger = logger;
+            this.mapper = mapper;
+        }
+
+
+    }
+}
