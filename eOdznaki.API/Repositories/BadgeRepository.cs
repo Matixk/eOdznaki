53e2499a6faf36647d3a6f24a7f2ff9c693bfd52
empyty <bartoszlistwan@gmail.com>
2019-03-07 11:46:14 +0100

    Created folder for repositories and badge repository for its interface

---
+++ eOdznaki.API/Repositories/BadgeRepository.cs
@@ -0,0 +1,25 @@
+﻿using eOdznaki.Models.Badges;
+using eOdznaki.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace eOdznaki.Repositories
+{
+    public class BadgeRepository : IBadgeRepository
+    {
+        public Badge AddBadge(Badge newBadge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Badge> GetAllBadges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Badge> GetBadgesByType(string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
