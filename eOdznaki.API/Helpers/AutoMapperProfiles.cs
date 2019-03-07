ba0381d8c38769a79cd7029dad7b2eddafcaeeb2
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-07 09:56:19 +0100

    Add automapper profiles

---
+++ eOdznaki.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace eOdznaki.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            
+        }
+    }
+}
