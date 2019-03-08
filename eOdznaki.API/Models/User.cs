9d144c84935f123f362aa95f39d5fdb345c14c80
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-08 10:05:47 +0100

    Update User model

--- eOdznaki.API/Models/User.cs
+++ eOdznaki.API/Models/User.cs
@@ -8,11 +8,13 @@ namespace eOdznaki.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
-        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
         public string AvatarUrl { get; set; }
 //      public IEnumerable<Badge> Badges { get; set; }
         public IEnumerable<UserRole> UserRoles { get; set; }
         public IEnumerable<Thread> UserThreads { get; set; }
-        public IEnumerable<Post> UsePosts { get; set; }
+        public IEnumerable<Post> UserPosts { get; set; }
     }
 }
