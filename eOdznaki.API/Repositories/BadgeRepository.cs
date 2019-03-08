36bdbf662726300904b94202e8049c8f01caf498
empyty <bartoszlistwan@gmail.com>
2019-03-08 11:08:15 +0100

    Changed methods to be async

--- eOdznaki.API/Repositories/BadgeRepository.cs
+++ eOdznaki.API/Repositories/BadgeRepository.cs
@@ -1,10 +1,12 @@
 ﻿using eOdznaki.Models.Badges;
 using eOdznaki.Persistence;
 using eOdznaki.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eOdznaki.Repositories
 {
@@ -19,22 +21,64 @@ namespace eOdznaki.Repositories
             this.logger = logger;
         }
 
-        public Badge AddBadge(Badge newBadge)
+        public async Task AddBadge(Badge newBadge)
         {
-            throw new NotImplementedException();
+            await context.AddAsync(newBadge);
+            await SaveAll();
         }
 
-        public IEnumerable<Badge> GetAllBadges()
+        public async Task<IEnumerable<Badge>> GetAllBadges()
         {
             logger.LogInformation("GetAllBadges was called");
-            return context.Badges.ToList();
+            return await context.Badges.ToListAsync();
         }
 
-        public IEnumerable<Badge> GetBadgesByType(string type)
+        public async Task<Badge> GetBadgeByType(BadgeTypeEnum type, int badgeId)
+        {
+            logger.LogInformation($"GetBadgeByType was called with parameter {type}");
+
+            switch (type)
+            {
+                case BadgeTypeEnum.BadgeDrop:
+                    return await context.Badges.OfType<BadgeDrops>().FirstOrDefaultAsync(b => b.Id == badgeId);
+
+                case BadgeTypeEnum.BadgeSummit:
+                    return await context.Badges.OfType<BadgeSummit>().FirstOrDefaultAsync(b => b.Id == badgeId);
+
+                case BadgeTypeEnum.BadgeTrail:
+                    return await context.Badges.OfType<BadgeTrails>().FirstOrDefaultAsync(b => b.Id == badgeId);
+
+                default:
+                    logger.LogWarning($"Couldn't find appropriate type for {type}");
+                    return new Badge();
+            }
+        }
+
+        public async Task<IEnumerable<Badge>> GetBadgesByType(BadgeTypeEnum type)
         {
             logger.LogInformation($"GetBadgesByType was called with parameter {type}");
-            // Only an example, create a switch statement checking type
-            return context.Badges.OfType<BadgeDrops>().ToList();
+
+            switch (type)
+            {
+                case BadgeTypeEnum.BadgeDrop:
+                    return await context.Badges.OfType<BadgeDrops>().ToListAsync();
+
+                case BadgeTypeEnum.BadgeSummit:
+                    return await context.Badges.OfType<BadgeSummit>().ToListAsync();
+
+                case BadgeTypeEnum.BadgeTrail:
+                    return await context.Badges.OfType<BadgeTrails>().ToListAsync();
+
+                default:
+                    logger.LogWarning($"Couldn't find appropriate type for {type}");
+                    return null;
+            }
+        }
+
+        public async Task SaveAll()
+        {
+            logger.LogInformation("SaveDatabaseInformation was called");
+            await context.SaveChangesAsync();
         }
     }
 }
