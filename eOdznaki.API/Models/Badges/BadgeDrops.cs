f26e302a8296bfb903393886191c97e14f4e371d
empyty <bartoszlistwan@gmail.com>
2019-03-08 11:06:11 +0100

    Changed variables in badge drops

--- eOdznaki.API/Models/Badges/BadgeDrops.cs
+++ eOdznaki.API/Models/Badges/BadgeDrops.cs
@@ -6,8 +6,10 @@ namespace eOdznaki.Models.Badges
     public class BadgeDrops : Badge
     {
         [Required]
-        public BadgeLevel BadgeLevel { get; set; }
+        public int BadgeLevel { get; set; }
         [Required]
         public int MaxLevel { get; private set; }
+        [Required]
+        public int ReachedHeight { get; set; }
     }
 }
