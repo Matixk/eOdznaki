a3562296ca832846c1eb2f802b552060e9adfc1f
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-08 10:06:39 +0100

    Add UserRepository

---
+++ eOdznaki.API/Persistence/Repositories/UsersRepository.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using eOdznaki.Models;
+using eOdznaki.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace eOdznaki.Persistence.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly DataContext context;
+
+        public UsersRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<User> GetUsers()
+        {
+            var users = context.Users.Include(u => u.UserThreads).AsQueryable();
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var query = context.Users.Include("UserThreads").Include("UserPosts").AsQueryable();
+
+            return await query.FirstOrDefaultAsync(u => u.Id == id);
+        }
+    }
+}
