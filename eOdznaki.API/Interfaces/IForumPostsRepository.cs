db22f95183c209db2a93f59c787f5ffaf32d06b7
matixk <matixk73@gmail.com>
2019-03-08 12:02:33 +0100

    IForumPostsRepository added

---
+++ eOdznaki.API/Interfaces/IForumPostsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using eOdznaki.Dtos.ForumPosts;
+using eOdznaki.Models;
+
+namespace eOdznaki.Interfaces
+{
+    public interface IForumPostsRepository
+    {
+        Task<IEnumerable<ForumPost>> GetAllForumPosts();
+        Task<ForumPost> GetForumPosts(int forumPostsId);
+        Task<ForumPost> Insert(ForumPostForCreateDto forumPost);
+        Task<ForumPost> Update(int userId, int forumPostId, ForumPostForUpdateDto forumPost);
+        Task<ForumPost> Delete(int userId, int forumPostId);
+    }
+}
