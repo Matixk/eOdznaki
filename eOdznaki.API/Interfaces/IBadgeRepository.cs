896413080edd80ee001a71ea16b457ab4641ebe7
empyty <bartoszlistwan@gmail.com>
2019-03-08 13:46:14 +0100

    Changed methods to async, added update methods

--- eOdznaki.API/Interfaces/IBadgeRepository.cs
+++ eOdznaki.API/Interfaces/IBadgeRepository.cs
@@ -10,7 +10,10 @@ namespace eOdznaki.Persistence.Repositories
         Task<IEnumerable<Badge>> GetBadgesByType(BadgeTypeEnum type);
         Task<Badge> GetBadgeByType(BadgeTypeEnum type, int badgeId);
 
-        Task AddBadge(Badge newBadge);
+        Task<Badge> AddBadge(Badge newBadge);
+        Task<Badge> UpdateBadgeLevelAsync(int badgeId, int newBadgeLevel, BadgeTypeEnum type);
+        Task<Badge> UpdateBadgeData(Badge updatedBadge);
+        Task<Badge> UpdateBadgeStatus(int badgeId, string newBadgeStatus);
 
         Task SaveAll();
     }
