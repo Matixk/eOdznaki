57eba8530b027efce786c8e76de85189ce2722eb
empyty <bartoszlistwan@gmail.com>
2019-03-11 10:26:07 +0100

    Added delete method; Save All now returns a boolean

--- eOdznaki.API/Interfaces/IBadgeRepository.cs
+++ eOdznaki.API/Interfaces/IBadgeRepository.cs
@@ -11,10 +11,11 @@ namespace eOdznaki.Persistence.Repositories
         Task<Badge> GetBadgeByType(BadgeTypeEnum type, int badgeId);
 
         Task<Badge> AddBadge(Badge newBadge);
-        Task<Badge> UpdateBadgeLevelAsync(int badgeId, int newBadgeLevel, BadgeTypeEnum type);
+        Task<Badge> UpdateBadgeLevel(int badgeId, int newBadgeLevel, BadgeTypeEnum type);
         Task<Badge> UpdateBadgeData(Badge updatedBadge);
         Task<Badge> UpdateBadgeStatus(int badgeId, string newBadgeStatus);
+        Task<bool> DeleteBadgeById(int id);
 
-        Task SaveAll();
+        Task<bool> SaveAll();
     }
 }
