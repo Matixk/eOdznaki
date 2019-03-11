378d0f006c9ab950f24dd7a5a669aa7175c0146b
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-11 11:55:57 +0100

    Add PagedList

---
+++ eOdznaki.API/Helpers/PagedList.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace eOdznaki.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        private PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+            AddRange(items);
+        }
+
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int PageSize { get; }
+        public int TotalCount { get; }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
