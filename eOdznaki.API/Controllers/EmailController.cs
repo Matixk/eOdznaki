b26ff7dc79b9277727e8ac3da5707a45e2e59c71
empyty <bartoszlistwan@gmail.com>
2019-03-11 11:03:38 +0100

    Added email controller to send emails

---
+++ eOdznaki.API/Controllers/EmailController.cs
@@ -0,0 +1,22 @@
+﻿using eOdznaki.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace eOdznaki.Controllers
+{
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailSender emailSender;
+        public EmailController(IEmailSender emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmailAsync([FromBody]string email, string subject, string message)
+        {
+            await emailSender.SendEmailAsync(email, subject, message);
+            return Ok();
+        }
+    }
+}
