93a9430f1b48205b22ec1270522698ff4c29d790
matixk <matixk73@gmail.com>
2019-03-11 14:53:00 +0100

    AnnouncementsRepository added

---
+++ eOdznaki.API/Interfaces/IAnnouncementsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using eOdznaki.Dtos.Announcements;
+using eOdznaki.Helpers;
+using eOdznaki.Helpers.Params;
+using eOdznaki.Models;
+
+namespace eOdznaki.Interfaces
+{
+    interface IAnnouncementsRepository
+    {
+        Task<PagedList<Announcement>> GetCurrentAnnouncements(AnnouncementsParams announcementsParams);
+        Task<Announcement> Insert(AnnouncementForCreateDto announcement);
+        Task<Announcement> Update(int announcementId, AnnouncementForUpdateDto announcement);
+        Task<Announcement> Delete(int announcementId);
+    }
+}
