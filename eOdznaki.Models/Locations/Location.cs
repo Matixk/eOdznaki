fc8d6f6f70c912c6ea3870759d905781d3fd3cad
empyty <bartoszlistwan@gmail.com>
2019-03-14 13:17:28 +0100

    Added location details, using geocoordinate

--- eOdznaki.Models/Locations/Location.cs
+++ eOdznaki.Models/Locations/Location.cs
@@ -1,7 +1,14 @@
-﻿namespace eOdznaki.Models.Locations
+﻿using GeoCoordinatePortable;
+using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Models.Locations
 {
-    public class Location
+    public class Location : GeoCoordinate
     {
         public int Id { get; private set; }
+
+        [Required] public string Name { get; private set; }
+
+        [Required] public GeoCoordinate Position { get; private set; }
     }
 }
