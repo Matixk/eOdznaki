d9eaf64ad0eb8cf8909e7dbb3bf3d1480ef3dc2d
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-14 13:47:37 +0100

    Add jwt module

--- eOdznaki-SPA/src/app/app.module.ts
+++ eOdznaki-SPA/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {TimeAgoPipe} from 'time-ago-pipe';
+import { TimeAgoPipe } from 'time-ago-pipe';
+import { JwtModule } from '@auth0/angular-jwt';
 
 
 import { AppComponent } from './app.component';
 
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -19,7 +23,14 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+         tokenGetter: tokenGetter,
+         whitelistedDomains: ['localhost:5000', 'da-app-106.azurewebsites.net'],
+         blacklistedRoutes: ['localhost:5000/api/auth', 'da-app-106.azurewebsites.net/api/auth']
+      }
+   })
   ],
   providers: [],
   bootstrap: [AppComponent]
