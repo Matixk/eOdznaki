4537b51f387a50475f3f87b8916255c5225f20ef
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-18 00:33:24 +0100

    Update app.module imports

--- eOdznaki-SPA/src/app/app.module.ts
+++ eOdznaki-SPA/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TimeAgoPipe } from 'time-ago-pipe';
-import { JwtModule } from '@auth0/angular-jwt';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TimeAgoPipe} from 'time-ago-pipe';
+import {JwtModule} from '@auth0/angular-jwt';
+import {CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {RegisterComponent} from './components/register/register.component';
+import {appRoutes} from './routes';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {ErrorInterceptorProvider} from './_services/error.interceptor';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -19,7 +26,11 @@ export function tokenGetter() {
 @NgModule({
   declarations: [
     AppComponent,
-    TimeAgoPipe
+    TimeAgoPipe,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     HttpClientModule,
@@ -29,19 +40,24 @@ export function tokenGetter() {
     FormsModule,
     ReactiveFormsModule,
     MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
       progressBar: true,
-   }),
+    }),
     JwtModule.forRoot({
       config: {
-         tokenGetter: tokenGetter,
-         whitelistedDomains: ['localhost:5000'],
-         blacklistedRoutes: ['localhost:5000/api/auth']
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
       }
-   }),
+    }),
   ],
-  providers: [],
+  providers: [
+    ErrorInterceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
