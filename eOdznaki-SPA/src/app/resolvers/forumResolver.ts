4ae2028b19a6828f5e5e86bf0b21c0092a26f0f2
matixk <matixk73@gmail.com>
2019-03-18 11:05:59 +0100

    Add Forum Resolver

---
+++ eOdznaki-SPA/src/app/resolvers/forumResolver.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {Resolve, ActivatedRouteSnapshot} from '@angular/router';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {Observable, of} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+import {Thread} from '../models/forum/thread';
+import {ThreadService} from '../_services/thread.service';
+
+@Injectable()
+export class ThreadsResolver implements Resolve<Thread[]> {
+  pageNumber = 1;
+  pageSize = 10;
+
+  constructor(
+    private threadService: ThreadService,
+    private router: Router,
+    private toastr: ToastrService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<Thread[]> {
+    return this.threadService.getThreads(this.pageNumber, this.pageSize).pipe(
+      catchError(error => {
+        this.toastr.error('Could not retrieve Threads data');
+        this.router.navigate(['/home']);
+        return of(null);
+      })
+    );
+  }
+}
