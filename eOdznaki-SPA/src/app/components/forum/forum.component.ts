9da3df45ed580ae591cc8a7976b0c0244e4c426e
matixk <matixk73@gmail.com>
2019-03-19 16:36:02 +0100

    Add search bar to forum navbar

--- eOdznaki-SPA/src/app/components/forum/forum.component.ts
+++ eOdznaki-SPA/src/app/components/forum/forum.component.ts
@@ -5,9 +5,11 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ThreadService} from '../../_services/thread.service';
 import {ToastrService} from 'ngx-toastr';
 import {PaginatedResult} from '../../models/pagination/paginatedResult';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {AuthService} from '../../_services/auth.service';
 import {ThreadForCreate} from '../../dtos/threadForCreate';
+import {SearchService} from '../../_services/search.service';
+import {FormValidatorOptions} from '../../utils/formValidatorOptions';
 
 @Component({
   selector: 'app-forum',
@@ -20,11 +22,14 @@ export class ForumComponent implements OnInit {
   pagination: Pagination;
 
   threadForm = new FormGroup({
-    title: new FormControl('', this.validatorOptions(true, 5, 50)),
-    content: new FormControl('', this.validatorOptions(true, 5, 2000))
+    title: new FormControl('', FormValidatorOptions.setStringOptions(true, 5, 50)),
+    content: new FormControl('', FormValidatorOptions.setStringOptions(true, 5, 2000))
   });
 
+  searchForm = new FormControl('', FormValidatorOptions.setStringOptions(true, 2, 25));
+
   constructor(private threadService: ThreadService,
+              private searchService: SearchService,
               private route: ActivatedRoute,
               private router: Router,
               private toastr: ToastrService,
@@ -37,15 +42,6 @@ export class ForumComponent implements OnInit {
     });
   }
 
-  validatorOptions(required: boolean, minLength: number, maxLength: number) {
-    const validators = [null, Validators.minLength(minLength), Validators.maxLength(maxLength)];
-    if (required) {
-      validators[0] = Validators.required;
-    }
-
-    return validators;
-  }
-
   pageChanged(event: any): void {
     this.pagination.currentPage = event.page;
     this.loadThreads();
@@ -55,6 +51,7 @@ export class ForumComponent implements OnInit {
     this.threadService.getThreads(this.pagination.currentPage, this.pagination.itemsPerPage)
       .subscribe((res: PaginatedResult<Thread[]>) => {
         this.threads = res.result;
+        console.log(this.threads[0].forumPosts);
         this.pagination = res.pagination;
       }, error => {
         this.toastr.error(error);
@@ -76,6 +73,12 @@ export class ForumComponent implements OnInit {
       }, () => {
         this.router.navigate(['/forum', threadId]);
       });
+    }
+  }
+
+  searchForum() {
+    if (this.searchForm.valid) {
+      this.router.navigate(['/search'], this.searchForm.value);
     }
   }
 
