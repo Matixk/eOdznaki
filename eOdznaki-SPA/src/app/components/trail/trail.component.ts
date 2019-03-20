54f8cd80dadfa98e4661e33ff6ba873ba5082d14
empyty <bartoszlistwan@gmail.com>
2019-03-20 15:38:16 +0100

    Created functions for adding origin and destination points

--- eOdznaki-SPA/src/app/components/trail/trail.component.ts
+++ eOdznaki-SPA/src/app/components/trail/trail.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {google} from '@agm/core/services/google-maps-types';
+import {Component, OnInit, ViewChild, ElementRef, NgZone} from '@angular/core';
+import {MapsAPILoader, LatLng} from '@agm/core';
+import {FormControl} from '@angular/forms';
+
+declare var google;
 
 @Component({
   selector: 'app-trail',
@@ -7,13 +10,21 @@ import {google} from '@agm/core/services/google-maps-types';
   styleUrls: ['./trail.component.scss']
 })
 export class TrailComponent implements OnInit {
-  lat = 51.678418;
-  lng = 7.809007;
+  default = {lat: 49.257794, lng: 19.919636};
+  zoom = 10;
+  public searchControl: FormControl;
   origin;
   destination;
   waypoints: any = [];
   selectedMarker;
   markers = [];
+  travelMode = 'WALKING';
+
+  @ViewChild('originInput')
+  public originElementRef: ElementRef;
+
+  @ViewChild('destinationInput')
+  public destinationElementRef: ElementRef;
 
   public renderOptions = {
     draggable: true,
@@ -23,43 +34,62 @@ export class TrailComponent implements OnInit {
     this.waypoints = event.request.waypoints;
   }
 
-  constructor() { }
+  constructor(private mapsAPILoader: MapsAPILoader,
+              private ngZone: NgZone) { }
 
   ngOnInit() {
+    this.searchControl = new FormControl();
+    this.mapsAPILoader.load().then(() => {
+      const autocompleteOrigin = new google.maps.places.Autocomplete(this.originElementRef.nativeElement);
+      autocompleteOrigin.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          const originPlace: google.maps.places.PlaceResult = autocompleteOrigin.getPlace();
+
+          if (originPlace.geometry === undefined || originPlace.geometry === null) {
+            return;
+          }
+
+          this.addOrigin(new google.maps.LatLng(originPlace.geometry.location.lat(), originPlace.geometry.location.lng()));
+          this.addMarker(originPlace.geometry.location.lat(), originPlace.geometry.location.lng());
+          this.default.lat = originPlace.geometry.location.lat();
+          this.default.lng = originPlace.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+      const autocompleteDestination = new google.maps.places.Autocomplete(this.destinationElementRef.nativeElement);
+      autocompleteDestination.addListener('place_changed', () => {
+        this.ngZone.run( () => {
+          const destinationPlace: google.maps.places.PlaceResult = autocompleteDestination.getPlace();
+
+          if (destinationPlace.geometry === undefined || destinationPlace.geometry === null) {
+            return;
+          }
+
+          this.addDestination(new google.maps.LatLng(destinationPlace.geometry.location.lat(), destinationPlace.geometry.location.lng()));
+        });
+      });
+    });
   }
 
   addMarker(lat: number, lng: number) {
     const latitude = lat;
     const longitude = lng;
+    const newLatLng = new google.maps.LatLng(latitude, longitude);
     if (this.origin == null) {
-      this.origin = {
-        lat: latitude,
-        lng: longitude
-      };
+      this.addOrigin(newLatLng);
+      console.log(this.origin);
     } else if (this.destination == null) {
-      this.destination = {
-        lat: latitude,
-        lng: longitude
-      };
+      this.addDestination(newLatLng);
+      console.log(this.destination);
     } else {
-      let oldLat = this.destination.lat;
-      let oldLng = this.destination.lng;
-      let newLocation = new google.maps.LatLng(oldLat, oldLng);
-        this.waypoints.push({ location: newLocation, stopover: false });
-        this.destination = {
-          lat: latitude,
-          lng: longitude
-        };
+      const oldLat = this.destination.lat;
+      const oldLng = this.destination.lng;
+      const newLocation = new google.maps.LatLng(oldLat, oldLng);
+      this.addWaypoint(newLocation);
+      this.addDestination(newLatLng);
       this.markers.push({ lat: latitude, lng: longitude, alpha: 1 });
+      console.log(this.waypoints);
     }
-    }
-
-  max(coordType: 'lat' | 'lng'): number {
-    return Math.max(...this.markers.map(marker => marker[coordType]));
-  }
-
-  min(coordType: 'lat' | 'lng'): number {
-    return Math.min(...this.markers.map(marker => marker[coordType]));
   }
 
   selectMarker(event) {
@@ -69,40 +99,16 @@ export class TrailComponent implements OnInit {
     };
   }
 
-  // openContextMenu(event) {
-  //   let menu = new BootstrapMenu('#mapMenu', {
-  //     actions: [{
-  //       name: 'Dodaj jako punkt startowy',
-  //       onClick: this.addOrigin(event)
-  //     }, {
-  //       name: 'Dodaj jako punkt docelowy',
-  //       onClick: this.addDestination(event)
-  //     }, {
-  //       name: 'Dodaj jako punkt pośredni',
-  //       onClick: this.addWaypoint(event)
-  //     }]
-  //   });
-  // }
-
-  addOrigin(event) {
-    this.origin = {
-      lat: event.latitude,
-      lng: event.longitude
-    };
+  addOrigin(data: LatLng) {
+    this.origin = data;
   }
 
-  addDestination(event) {
-    this.destination = {
-      lat: event.latitude,
-      lng: event.longitude
-    };
+  addDestination(data: LatLng) {
+    this.destination = data;
   }
 
-  addWaypoint(event) {
-    this.waypoints.push(event.latitude, event.longitude, 1);
-  }
-
-  getDirection() {
+  addWaypoint(data: LatLng) {
+    this.waypoints.push({location: data});
   }
 
 }
