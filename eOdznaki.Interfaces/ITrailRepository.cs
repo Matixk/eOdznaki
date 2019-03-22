7dbca61afbcee2d9b48d9963a68643edde38618f
empyty <bartoszlistwan@gmail.com>
2019-03-22 15:15:55 +0100

    Update ITrailRepository.cs

---
+++ eOdznaki.Interfaces/ITrailRepository.cs
@@ -0,0 +1,15 @@
+﻿using eOdznaki.Helpers;
+using eOdznaki.Helpers.Params;
+using eOdznaki.Models.Trails;
+using System.Threading.Tasks;
+
+namespace eOdznaki.Interfaces
+{
+    public interface ITrailRepository
+    {
+        Task<PagedList<Trail>> GetAllTrailsAsync(TrailsParams trailsParams);
+        Task<Trail> GetTrail(int trailId);
+        Task<Trail> Add(Trail trail);
+        Task<Trail> Delete(int trailId);
+    }
+}
