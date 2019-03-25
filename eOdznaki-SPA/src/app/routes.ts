aa393f511c9dcf70ba301f029d935b250f8ad7d3
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-03-25 14:37:58 +0100

    Add userRoles route

--- eOdznaki-SPA/src/app/routes.ts
+++ eOdznaki-SPA/src/app/routes.ts
@@ -6,6 +6,8 @@ import {ProfileEditComponent} from './components/profile-edit/profile-edit.compo
 import {PreventUnsavedChanged} from './_guards/prevent-unsaved-changes.guard';
 import {ProfileEditResolver} from './resolvers/profile-edit-resolver';
 import {AuthGuard} from './_guards/auth.guard';
+import {UserRolesComponent} from './components/user-roles/user-roles.component';
+import {UserRolesResolver} from './resolvers/user-roles-resolver';
 
 export const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -20,6 +22,11 @@ export const appRoutes: Routes = [
         path: 'profile/edit', component: ProfileEditComponent,
         resolve: {user: ProfileEditResolver}, canDeactivate: [PreventUnsavedChanged],
       },
+      {
+        path: 'manage/roles', component: UserRolesComponent,
+        resolve: { users: UserRolesResolver },
+        data: { roles: ['Admin'] }
+      }
     ]
   },
   {path: '**', redirectTo: '', pathMatch: 'full'},
