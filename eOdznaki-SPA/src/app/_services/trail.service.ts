331bddd6cc3008811ee3a6ce794b811eadd72079
empyty <bartoszlistwan@gmail.com>
2019-03-25 11:21:53 +0100

    Added get and post methods

--- eOdznaki-SPA/src/app/_services/trail.service.ts
+++ eOdznaki-SPA/src/app/_services/trail.service.ts
@@ -3,6 +3,11 @@ import {environment} from '../../environments/environment';
 import {JwtHelperService} from '@auth0/angular-jwt';
 import {User} from '../models/user';
 import {HttpClient} from '@angular/common/http';
+import {Trail} from '../dtos/trail';
+import {PaginatedResult} from '../models/pagination/paginatedResult';
+import {Observable} from 'rxjs';
+import {HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable ({
   providedIn: 'root'
@@ -14,5 +19,37 @@ export class TrailService {
   currentUser: User;
 
   constructor(private http: HttpClient) {
+  }
+
+  getTrail(id: number): Observable<Trail> {
+    return this.http.get<Trail>(`${this.baseUrl}/${id}`);
+  }
+
+  getTrails(page?, itemsPerPage?): Observable<PaginatedResult<Trail[]>> {
+    const paginatedResult: PaginatedResult<Trail[]> = new PaginatedResult<Trail[]>();
+
+    let params = new HttpParams();
+
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http.get<Trail[]>(this.baseUrl, { observe: 'response', params })
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          if (response.headers.get('Pagination') != null) {
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+          }
+          return paginatedResult;
+        })
+      );
+  }
+
+  addTrail(trail: Trail): Observable<Trail> {
+    console.log(trail);
+
+    return this.http.post<Trail>(this.baseUrl, trail);
   }
 }
