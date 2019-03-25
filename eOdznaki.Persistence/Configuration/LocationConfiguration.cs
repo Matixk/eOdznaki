d3c481dfae8ae73a136e72d0ccb83e82b221dd3d
empyty <bartoszlistwan@gmail.com>
2019-03-25 14:59:35 +0100

    Added location configuration

---
+++ eOdznaki.Persistence/Configuration/LocationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using eOdznaki.Models.Locations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eOdznaki.Persistence.Configuration
+{
+    class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Location> builder)
+        {
+        }
+    }
+}
