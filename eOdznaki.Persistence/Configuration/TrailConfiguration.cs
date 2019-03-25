ae159ffb466179b064eb933c54267ec77f32d0dc
empyty <bartoszlistwan@gmail.com>
2019-03-25 14:59:22 +0100

    Added trail configuration

---
+++ eOdznaki.Persistence/Configuration/TrailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using eOdznaki.Models.Trails;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eOdznaki.Persistence.Configuration
+{
+    class TrailConfiguration : IEntityTypeConfiguration<Trail>
+    {
+        public void Configure(EntityTypeBuilder<Trail> builder)
+        {
+            builder.HasOne(t => t.StartPoint).WithOne().OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(t => t.EndPoint).WithOne().OnDelete(DeleteBehavior.Restrict);
+            builder.HasMany(t => t.Checkpoints).WithOne().OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
