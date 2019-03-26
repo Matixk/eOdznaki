3829a7361578d1608cd2dedc2fa1f118272decf7
empyty <bartoszlistwan@gmail.com>
2019-03-26 15:42:44 +0100

    Added trail dto without an id

---
+++ eOdznaki.Dtos/TrailDto.cs
@@ -0,0 +1,15 @@
+﻿using eOdznaki.Models.Locations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eOdznaki.Dtos
+{
+    public class TrailDto
+    {
+        public Location StartPoint { get; private set; }
+
+        public Location EndPoint { get; private set; }
+
+        public IEnumerable<Location> Checkpoints { get; private set; }
+    }
+}
