345d1d1ff5e8582c8fe61e7356c1f73534c7adf9
Maziarella <kiebala.magdalena@gmail.com>
2019-03-26 14:20:39 +0100

    Implement authentication by facebook account in CustomAuthentictionConfiguration class.

--- eOdznaki.API/Configuration/CustomAuthenticationConfiguration.cs
+++ eOdznaki.API/Configuration/CustomAuthenticationConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
@@ -7,8 +8,13 @@ namespace eOdznaki.Configuration
 {
     public static class CustomAuthenticationConfiguration
     {
-        public static void AddCustomAuthentication(this IServiceCollection services, string token)
+        public static void AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
+            var token = configuration.GetSection("Secrets:FacebookAppId").Value;
+            var fbId = configuration.GetSection("Secrets:FacebookAppId").Value;
+            var fbSecret = configuration.GetSection("Secrets:FacebookAppKey").Value;
+
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -20,7 +26,13 @@ namespace eOdznaki.Configuration
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
+                })
+                .AddFacebook(options =>
+                {
+                    options.AppId = fbId;
+                    options.AppSecret = fbSecret;
                 });
+
         }
     }
 }
