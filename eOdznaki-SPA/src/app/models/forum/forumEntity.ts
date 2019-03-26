3e126a414feed156f5e85a625d2815c51b182f35
matixk <matixk73@gmail.com>
2019-03-26 17:38:56 +0100

    Update search display

--- eOdznaki-SPA/src/app/models/forum/forumEntity.ts
+++ eOdznaki-SPA/src/app/models/forum/forumEntity.ts
@@ -1,7 +1,10 @@
+import {User} from '../user/user';
+
 export interface ForumEntity {
   id: number;
   authorId: number;
   authorName: string;
   authorAvatar: string;
   created: Date;
+  author: User;
 }
