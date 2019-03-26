f043ae689742e25b3b9a6ebb49a5941619b2ec61
Bartłomiej Żądło <bartlomiejzadlo@gmail.com>
2019-03-26 15:23:49 +0100

    Remove unused imports

--- eOdznaki-SPA/src/app/components/facebook-login/facebook-login.component.ts
+++ eOdznaki-SPA/src/app/components/facebook-login/facebook-login.component.ts
@@ -1,15 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Thread } from '../../models/forum/thread';
-import { Pagination } from '../../models/pagination/pagination';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ThreadService } from '../../_services/thread.service';
-import { ToastrService } from 'ngx-toastr';
-import { PaginatedResult } from '../../models/pagination/paginatedResult';
-import { FormControl, FormGroup } from '@angular/forms';
-import { AuthService } from '../../_services/auth.service';
-import { ThreadForCreate } from '../../dtos/threadForCreate';
-import { SearchService } from '../../_services/search.service';
-import { FormValidatorOptions } from '../../utils/formValidatorOptions';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-facebook-login',
