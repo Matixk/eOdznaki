c55906c46263576315e4eed564cb6fe9444b8cf8
empyty <bartoszlistwan@gmail.com>
2019-03-26 15:43:59 +0100

    Added constructors and removed required annotations

--- eOdznaki.Models/Trails/Trail.cs
+++ eOdznaki.Models/Trails/Trail.cs
@@ -6,11 +6,27 @@ namespace eOdznaki.Models.Trails
 {
     public class Trail
     {
+        public Trail()
+        {}
+
+        public Trail(Location origin, Location destination, IEnumerable<Location> checkpoints)
+        {
+            StartPoint = origin;
+            EndPoint = destination;
+            Checkpoints = checkpoints;
+        }
+
+        public Trail(Location origin, Location destination)
+        {
+            StartPoint = origin;
+            EndPoint = destination;
+        }
+
         public int Id { get; private set; }
 
-        [Required] public Location StartPoint { get; private set; }
+        public Location StartPoint { get; private set; }
 
-        [Required] public Location EndPoint { get; private set; }
+        public Location EndPoint { get; private set; }
 
         public IEnumerable<Location> Checkpoints { get; private set; }
 
